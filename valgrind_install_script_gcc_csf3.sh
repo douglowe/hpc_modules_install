2aea9f574732202b405ff776419bb294da65f8a9
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2019-01-28 11:48:02 +0000

    Added valgrind installation script.

---
+++ valgrind_install_script_gcc_csf3.sh
@@ -0,0 +1,206 @@
+cat /etc/redhat-release
+
+# Location of final root directory
+APPROOT=/mnt/iusers01/support/mbessdl2/privatemodules_packages/csf3/apps/valgrind
+
+APPVER=3.14.0
+APPDIR=$APPROOT/$APPVER
+
+
+
+#sudo mkdir $APPROOT
+#sudo chown ${USER}. $APPROOT
+mkdir $APPROOT
+
+
+cd $APPROOT
+mkdir $APPVER archive build
+cd archive
+
+module load tools/env/proxy2
+
+
+wget http://www.valgrind.org/downloads/valgrind-${APPVER}.tar.bz2
+
+
+cd ../build
+tar xf ../archive/valgrind-${APPVER}.tar.bz2
+
+cd valgrind-${APPVER}
+
+
+./configure --prefix=$APPDIR 2>&1 | tee ../config-$APPVER.log
+make 2>&1 | tee make-$APPVER.log
+make install 2>&1 | tee make-install-$APPVER.log
+
+
+#sudo chmod -R og+rX $APPROOT
+chmod -R og+rX $APPROOT
+
+# module file location
+MDIR=/mnt/iusers01/support/mbessdl2/privatemodules/priv_apps/valgrind
+
+
+#sudo mkdir $MDIR
+#sudo chown ${USER}. $MDIR
+mkdir $MDIR
+
+cd $MDIR
+
+MPATH=priv_apps/valgrind/${APPVER}
+
+
+
+
+echo "#%Module1.0####################################################
+##
+## CSF3 LIBRARY-TEMPLATE Modulefile
+##
+##
+proc getenv {key {defaultvalue {}}} {
+  global env; expr {[info exist env(\$key)]?\$env(\$key):\$defaultvalue}
+}
+
+proc ModulesHelp { } {
+    global APPVER APPNAME APPURL APPCSFURL COMPVER COMPNAME
+
+    puts stderr \"
+    Adds \$APPNAME \$APPVER to your PATH environment variable and any necessary
+    libraries. It has been compiled with the \$COMPNAME \$COMPVER compiler.
+
+    For information on how to run \$APPNAME on the CSF please see:
+    \$APPCSFURL
+    
+    For application specific info see:
+    \$APPURL
+\"
+}
+
+set    APPVER         ${APPVER}
+set    APPNAME        netcdf
+set    APPNAMECAPS    NETCDF
+set    APPURL        http://www.unidata.ucar.edu/software/netcdf/
+set    APPCSFURL    http://ri.itservices.manchester.ac.uk/csf3/software/libraries/$APPNAME
+# Default gcc will be
+set    COMPVER        4.8.5
+set    COMPNAME    gcc
+set    COMPDIR        \${COMPNAME}
+
+module-whatis    \"Adds \$APPNAME \$APPVER to your environment\"
+
+# Do we want to ensure the user (or another modulefile) has loaded the compiler?
+# Can be a dirname (any modulefile from that dir) or a specific version.
+# Multiple names on one line mean this OR that OR theothere
+# Multiple prereq lines mean prereq this AND prepreq that AND prereq theother
+#prereq  priv_libs/\$COMPNAME/zlib/1.2.11
+
+# Do we want to prohibit use of other modulefiles (similar rules to above)
+# conflict libs/SOMELIB/older.version
+
+# Do we want to load dependency modulefiles on behalf of the user?
+# You MIGHT HAVE TO REMOVE THE prereq MODULEFILES FROM ABOVE
+# module load libs/otherlib/7.8.9
+# module load ......
+module load priv_libs/\$COMPNAME/zlib/1.2.11
+module load priv_libs/\$COMPNAME/hdf5/1.8.21
+
+set     APPDIR    /mnt/iusers01/support/mbessdl2/privatemodules_packages/csf3/libs/\$COMPNAME/\$APPNAME/\$APPVER
+
+setenv        \${APPNAMECAPS}DIR      \$APPDIR
+setenv        \${APPNAMECAPS}_HOME    \$APPDIR
+setenv        \${APPNAMECAPS}BIN      \$APPDIR/bin
+setenv        \${APPNAMECAPS}LIB      \$APPDIR/lib
+setenv        \${APPNAMECAPS}INCLUDE  \$APPDIR/include
+
+# Typical env vars to help a compiler find this library and header files
+# and to also allow the library to be found when your compiled code is run.
+prepend-path    C_INCLUDE_PATH    \$APPDIR/include
+prepend-path    CPATH             \$APPDIR/include
+prepend-path    LIBRARY_PATH      \$APPDIR/lib
+prepend-path    LD_LIBRARY_PATH   \$APPDIR/lib
+prepend-path    PATH              \$APPDIR/bin
+prepend-path    MANPATH           \$APPDIR/share/man
+# Add any other vars your need...
+" > $APPVER
+
+
+
+
+
+
+
+echo "#%Module1.0####################################################
+##
+## CSF3 APP-TEMPLATE Modulefile
+##
+##
+proc getenv {key {defaultvalue {}}} {
+  global env; expr {[info exist env(\$key)]?$env(\$key):\$defaultvalue}
+}
+
+# If using MPI and or CUDA, add MPIVER and CUVER with suitable text
+proc ModulesHelp { } {
+    global APPVER APPNAME APPCSFURL APPURL COMPVER COMPNAME
+
+    puts stderr \"
+    Adds \$APPNAME \$APPVER to your PATH environment variable and any necessary
+    dependent modulefiles.
+    
+    It has been compiled with the \$COMPNAME \$COMPVER compiler.
+
+    For information on how to run \$APPNAME on the CSF please see:
+    \$APPCSFURL
+    
+    For application specific info see:
+    \$APPURL
+\"
+}
+
+set    APPVER         ${APPVER}
+set    APPNAME        valgrind
+set    APPNAMECAPS    VALGRIND
+set    APPCSFURL    http://ri.itservices.manchester.ac.uk/csf3/software/applications/$APPNAME
+set    APPURL        http://valgrind.org/
+# Default gcc will be
+set    COMPVER        4.8.5
+set    COMPNAME    gcc
+set    COMPDIR        \${COMPNAME}
+
+module-whatis    \"Adds \$APPNAME \$APPVER to your environment\"
+
+# Do we want to ensure the user (or another modulefile) has loaded the compiler?
+# Can be a dirname (any modulefile from that dir) or a specific version.
+# Multiple names on one line mean this OR that OR theothere
+# Multiple prereq lines mean prereq this AND prepreq that AND prereq theother
+# prereq compilers/$COMPNAME
+
+# Do we want to prohibit use of other modulefiles (similar rules to above)
+# conflict libs/SOMELIB/older.version
+
+# Do we want to load dependency modulefiles on behalf of the user?
+# You MIGHT HAVE TO REMOVE THE prereq MODULEFILES FROM ABOVE
+#
+# module load mpi/$COMPDIR/openmpi/$MPIVER
+# module load libs/cuda/$CUVER
+#
+# module load libs/otherlib/7.8.9
+# module load ......
+
+set     APPDIR           /mnt/iusers01/support/mbessdl2/privatemodules_packages/csf3/apps/\$APPNAME/\$APPVER
+
+setenv  \${APPNAMECAPS}DIR      \$APPDIR
+setenv  \${APPNAMECAPS}_HOME    \$APPDIR
+setenv  \${APPNAMECAPS}INCLUDE  \$APPDIR/include
+
+
+# Typical env vars needed to run an app
+
+prepend-path    C_INCLUDE_PATH    \$APPDIR/include
+prepend-path    CPATH             \$APPDIR/include
+prepend-path    PATH              \$APPDIR/bin
+prepend-path    LIBRARY_PATH      \$APPDIR/lib
+prepend-path    LD_LIBRARY_PATH   \$APPDIR/lib
+prepend-path    MANPATH           \$APPDIR/share/man
+# Add any other vars you need...
+" > $APPVER
+
