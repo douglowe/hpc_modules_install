8e2639be4542a56eefeafdb3d6e3a2a04c99ed13
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2023-06-01 16:47:30 +0100

    csf3/intel netcdf update to 4.9.2

--- CSF3/netcdf_install_script_intel_csf3.sh
+++ CSF3/netcdf_install_script_intel_csf3.sh
@@ -1,12 +1,15 @@
 cat /etc/redhat-release
 
 # Location of final root directory
-#INROOT=/opt/apps/libs/
-INROOT=/mnt/iusers01/support/mbessdl2/privatemodules_packages/csf3/libs
-APPROOT=$INROOT/intel/netcdf
+INROOT=/opt/apps/libs/
+#INROOT=/mnt/iusers01/support/mbessdl2/privatemodules_packages/csf3/libs
+APPROOT=$INROOT/intel-19.1/netcdf
 
-APPVER=4.6.2
+APPVER=4.9.2
 APPDIR=$APPROOT/$APPVER
+
+# netcdf-fortran version number is different to netcdf-c version
+FORTVER=4.6.1
 
 
 #sudo mkdir $APPROOT
@@ -17,45 +20,49 @@ mkdir $APPROOT
 cd $APPROOT
 mkdir $APPVER archive build
 cd archive
+mkdir netcdf-c netcdf-fortran
+
 
 module load tools/env/proxy2
 
-wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-c-${APPVER}.tar.gz
+cd netcdf-c
+wget https://github.com/Unidata/netcdf-c/archive/refs/tags/v${APPVER}.tar.gz
+cd ../netcdf-fortran
+wget https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v${FORTVER}.tar.gz
 
-cd ../build
-tar xzf ../archive/netcdf-c-${APPVER}.tar.gz
+cd ../../build
+tar xzf ../archive/netcdf-c/v${APPVER}.tar.gz
 
 cd netcdf-c-${APPVER}
 
 
-module load compilers/intel/17.0.7
-module load use.own
-#module load priv_libs/gcc/zlib/1.2.11
-module load priv_libs/intel/hdf5/1.8.21
-#module load libs/gcc/zlib/1.2.11
-#module load libs/intel/hdf5/1.8.21
+#module load use.own
+module load compilers/intel/19.1.2
+#module load priv_libs/intel-18.0/zlib/1.2.13
+#module load priv_libs/intel-18.0/hdf5/1.14.1
+module load libs/intel-19.1/zlib/1.2.13
+module load libs/intel-19.1/hdf5/1.14.1
 
 
 export LDFLAGS=-L$HDF5LIB
 export CPPFLAGS=-I$HDF5INCLUDE
 
-# current state of --enable-remote-fortran-bootstrap is not useable (doesn't seem to carry config settings from main script), 
+# 1) using --disable-nczarr to remove extension mapping to key-value pair cloud storage systems 
+#    see https://docs.unidata.ucar.edu/netcdf/NUG/nczarr_head.html for more information
+# 2) current state of --enable-remote-fortran-bootstrap is not useable (doesn't seem to carry config settings from main script), 
 #    so we will replicate the process manually below. Check at future date to see if it is more useable.
 #./configure --prefix=$APPDIR --enable-remote-fortran-bootstrap --enable-large-file-tests 2>&1 | tee ../config-$APPVER.log
-./configure --prefix=$APPDIR --enable-large-file-tests 2>&1 | tee ../config-$APPVER.log
+./configure --prefix=$APPDIR --disable-nczarr --enable-large-file-tests 2>&1 | tee ../config-$APPVER.log
 make 2>&1 | tee make-$APPVER.log
 make check 2>&1 | tee make-check-$APPVER.log
 make install 2>&1 | tee make-install-$APPVER.log
 
 
 # installing fortran libraries
-module load services/git
-
-TARGVERSION="v4.4.5"
-
-git clone http://github.com/unidata/netcdf-fortran
+mkdir netcdf-fortran
 cd netcdf-fortran
-git checkout $TARGVERSION
+
+tar xvf ../../../archive/netcdf-fortran/v${FORTVER}.tar.gz --strip-components=1 
 
 # need to add the netcdf lib and include paths too - so make sure to run make install above before doing this!
 export CPPFLAGS="-I$HDF5INCLUDE -I$APPDIR/include"
@@ -66,15 +73,13 @@ export LDFLAGS="-L$HDF5LIB -L$APPDIR/lib"
 #      (while it is empty for the standard gnu compiler), so we need to append this 
 #      path information, rather than just create this env variable fresh)
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH;$APPDIR/lib;$HDF5LIB"
+export HDF5_PLUGIN_PATH="$HDF5LIB"
 
-./configure --prefix=$APPDIR --enable-large-file-tests 2>&1 | tee config-$APPVER.log
+
+# zstandard plugin is failing tests, so disable for the moment
+./configure --prefix=$APPDIR --disable-zstandard-plugin --enable-large-file-tests 2>&1 | tee config-$APPVER.log
 make 2>&1 | tee make-$APPVER.log
-# There have been a lot of changes to the checks since 4.4.5 was released in January 
-#    (only 2 months ago!!!). In 4.4.5 there are a number of issues in make check (not make),
-#    which are solved by those changes (some code corrections, some deletions of old tests).
-#    Don't run make check at the moment - but do revisit this step when the next release 
-#    comes out!!
-#make check 2>&1 | tee make-check-$APPVER.log
+make check 2>&1 | tee make-check-$APPVER.log
 make install 2>&1 | tee make-install-$APPVER.log
 
 
@@ -85,8 +90,8 @@ make install 2>&1 | tee make-install-$APPVER.log
 chmod -R og+rX $APPROOT
 
 # module file location
-MDIR=/mnt/iusers01/support/mbessdl2/privatemodules/priv_libs/intel/netcdf
-#MDIR=/opt/apps/modules/libs/gcc/netcdf
+#MDIR=/mnt/iusers01/support/mbessdl2/privatemodules/priv_libs/intel-18.0/netcdf
+MDIR=/opt/apps/modules/libs/intel-19.1/netcdf
 
 
 #sudo mkdir $MDIR
@@ -95,8 +100,8 @@ mkdir $MDIR
 
 cd $MDIR
 
-#MPATH=libs/intel/netcdf/${APPVER}
-MPATH=priv_libs/intel/netcdf/${APPVER}
+MPATH=libs/intel-19.1/netcdf/${APPVER}
+#MPATH=priv_libs/intel-18.0/netcdf/${APPVER}
 
 
 
@@ -131,9 +136,9 @@ set    APPNAMECAPS    NETCDF
 set    APPURL        http://www.unidata.ucar.edu/software/netcdf/
 set    APPCSFURL    http://ri.itservices.manchester.ac.uk/csf3/software/libraries/$APPNAME
 # Default gcc will be
-set    COMPVER        17.0.7
+set    COMPVER        19.1.2
 set    COMPNAME    intel
-set    COMPDIR        \${COMPNAME}
+set    COMPDIR        \${COMPNAME}-19.1
 
 module-whatis    \"Adds \$APPNAME \$APPVER to your environment\"
 
@@ -141,9 +146,11 @@ module-whatis    \"Adds \$APPNAME \$APPVER to your environment\"
 # Do we want to prohibit use of other modulefiles (similar rules to above)
 # conflict libs/SOMELIB/older.version
 
-module load libs/\$COMPNAME/hdf5/1.8.21
+module load compilers/\${COMPNAME}/\${COMPVER}
+module load libs/\$COMPDIR/zlib/1.2.13
+module load libs/\$COMPDIR/hdf5/1.14.1
 
-set     APPDIR    $INROOT/\$COMPNAME/\$APPNAME/\$APPVER
+set     APPDIR    $INROOT/\$COMPDIR/\$APPNAME/\$APPVER
 
 setenv        \${APPNAMECAPS}DIR      \$APPDIR
 setenv        \${APPNAMECAPS}_HOME    \$APPDIR
