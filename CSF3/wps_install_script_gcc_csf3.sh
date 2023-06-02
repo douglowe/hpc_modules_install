7a12c328021041997b90d82b86840cc79dacd61b
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2023-06-02 17:39:03 +0100

    csf/gcc wps updated to 4.5

--- CSF3/wps_install_script_gcc_csf3.sh
+++ CSF3/wps_install_script_gcc_csf3.sh
@@ -8,7 +8,8 @@
 ## configuration settings
 INROOT=/opt/apps/apps
 
-APPVER=4.0.3
+APPVER=4.5
+COMPILER=gcc
 
 # settings for modules file
 MDIR=/opt/apps/modules/apps/gcc/wps
@@ -30,26 +31,32 @@ mkdir $APPROOT
 
 # making the executable code directory, and build directory (no archive for this)
 cd $APPROOT
-mkdir $APPVER build
-cd build
+mkdir $APPVER build archive
+cd archive
 
 # download the WRF code
 module load tools/env/proxy2
-git clone https://github.com/UoMResearchIT/WPS.git
+#git clone https://github.com/UoMResearchIT/WPS.git
+wget https://github.com/wrf-model/WPS/archive/refs/tags/v${APPVER}.tar.gz
+
+
+cd ../build
+
+tar zxf ../archive/v${APPVER}.tar.gz
 
 # create link to WRF compilation directory
 ln -s $WRFDIR WRF
 
-# checkout the specific version that we want
-cd WPS
-git checkout v$APPVER
+# go to build directory
+cd WPS-${APPVER}
 
 # load modules needed for compiling the code
 # (loading netcdf should load hdf5 and zlib libraries too)
-module load libs/gcc/netcdf/4.6.2
-module load mpi/gcc/openmpi/3.1.4
-module load libs/gcc/jasper/2.0.14 
-module load libs/gcc/libpng/1.6.36
+module load compilers/gcc/8.2.0
+module load libs/${COMPILER}/jasper/2.0.33
+module load libs/${COMPILER}/libpng/1.6.39
+module load libs/${COMPILER}/netcdf/4.9.2
+module load mpi/${COMPILER}/openmpi/4.1.2-gcc-8.2.0
 
 
 # environmental settings
@@ -69,15 +76,15 @@ export MPI_LIB=
 # create operational directories, and copy executables (including utilities)
 cd ../../$APPVER
 mkdir bin
-cp ../build/WPS/*.exe ../build/WPS/*.csh bin/
-cp ../build/WPS/util/*exe bin/
+cp ../build/WPS-${APPVER}/*.exe ../build/WPS-${APPVER}/*.csh bin/
+cp ../build/WPS-${APPVER}/util/*exe bin/
 
 # create running directory template directory, and copy necessary setting files
 mkdir run_dir
 mkdir run_dir/geogrid run_dir/ungrib run_dir/metgrid
-cp -a ../build/WPS/geogrid/GEOGRID.TBL* run_dir/geogrid/
-cp -a ../build/WPS/ungrib/Variable_Tables run_dir/ungrib/
-cp -a ../build/WPS/metgrid/METGRID.TBL* run_dir/metgrid/
+cp -a ../build/WPS-${APPVER}/geogrid/GEOGRID.TBL* run_dir/geogrid/
+cp -a ../build/WPS-${APPVER}/ungrib/Variable_Tables run_dir/ungrib/
+cp -a ../build/WPS-${APPVER}/metgrid/METGRID.TBL* run_dir/metgrid/
 
 
 ### need some code creating the example batch scripts here???
@@ -132,18 +139,19 @@ set    APPNAMECAPS    WPS
 set    APPURL        http://www2.mmm.ucar.edu/wrf/users/
 set    APPCSFURL     http://ri.itservices.manchester.ac.uk/csf3/software/applications/\$APPNAME
 # Default gcc will be
-set    COMPVER        4.8.5
+set    COMPVER        8.2.0
 set    COMPNAME    gcc
 set    COMPDIR        \${COMPNAME}
-set    MPIVER         3.1.4
+set    MPIVER         4.1.2
 
 module-whatis    \"Adds \$APPNAME \$APPVER to your environment\"
 
 # load required modules
-module load libs/\$COMPNAME/netcdf/4.6.2
-module load mpi/\$COMPNAME/openmpi/3.1.4
-module load libs/\$COMPNAME/jasper/2.0.14 
-module load libs/\$COMPNAME/libpng/1.6.36
+module load compilers/\$COMPNAME/\$COMPVER
+module load libs/\$COMPNAME/netcdf/4.9.2
+module load mpi/\$COMPNAME/openmpi/4.1.2-gcc-8.2.0
+module load libs/\$COMPNAME/jasper/2.0.33
+module load libs/\$COMPNAME/libpng/1.6.39
 
 set     APPDIR    $INROOT/\$COMPNAME/\$APPNAME/\$APPVER
 
